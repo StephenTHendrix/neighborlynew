c0eb101f34f93f84732a5dc9eba26ea583af3374
StephenTHendrix <stephenthendrix@gmail.com>
2019-12-05 12:40:55 -0600

    root static

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.use('/event/', Events)
 const db = require("./models");
 require("./routes/event.js")(app);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('/'));
+}
 
 
 app.post("/api", upload.single("./client/public/images", 12), function (req, res) {
